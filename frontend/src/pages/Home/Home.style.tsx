4d4022e4a12a27588136076b760981eaadf149d7
antkahn <ant.kahn@gmail.com>
2019-04-02 12:03:33 +0200

    change home style

--- frontend/src/pages/Home/Home.style.tsx
+++ frontend/src/pages/Home/Home.style.tsx
@@ -3,5 +3,9 @@ import styled from 'styled-components';
 export default {
   Intro: styled.div`
     font-size: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
   `,
 };
