54bd30347053a8ac3a992e4c5092b96b70185f5b
antkahn <ant.kahn@gmail.com>
2019-04-03 09:42:05 +0200

    remove github URL

--- frontend/src/services/networking/request.ts
+++ frontend/src/services/networking/request.ts
@@ -1,22 +1,21 @@
 import request from 'superagent';
 
-const baseUrl = 'https://api.github.com';
 const backendBaseUrl = process.env.REACT_APP_API_BASE_URL || '';
 
 export const makeGetRequest = async (endpoint: string, data: {} | null = null) => {
   if (data === null) {
-    return request.get(`${baseUrl}${endpoint}`).set('Accept', 'application/json');
+    return request.get(`${backendBaseUrl}${endpoint}`).set('Accept', 'application/json');
   }
 
   return request
-    .get(`${baseUrl}${endpoint}`)
+    .get(`${backendBaseUrl}${endpoint}`)
     .query(data)
     .set('Accept', 'application/json');
 };
 
 export const makePostRequest = (endpoint: string, data: {}) =>
   request
-    .post(`${baseUrl}${endpoint}`)
+    .post(`${backendBaseUrl}${endpoint}`)
     .send(data)
     .set('Accept', 'application/json');
 
