ec77788c77a20bcb23e7213c4bf34a2914693481
antkahn <ant.kahn@gmail.com>
2019-04-03 09:41:33 +0200

    display api fetched pokemons

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,11 +2,33 @@ import * as React from 'react';
 
 import Pokemon from 'components/Pokemon';
 import Style from './Home.style';
+import { makeGetRequest } from 'services/networking/request';
 
-class Home extends React.Component {
-  render(): React.ReactNode {
+interface Props {}
+interface State {
+  pokemons: {
+    id: number;
+    name: string;
+  }[];
+}
+
+class Home extends React.Component<Props, State> {
+  state: State = {
+    pokemons: [],
+  };
+
+  componentDidMount() {
+    makeGetRequest('/pokemon').then(({ body: pokemons }) => this.setState({ pokemons }));
+  }
+
+  render() {
+    const { pokemons } = this.state;
+    if (!pokemons.length) return null;
+    const [pokemon] = this.state.pokemons;
+
     return (
       <Style.Intro>
+        <Pokemon name={pokemon.name} id={pokemon.id} />
         <Pokemon name="Carapuce" id={7} />
         <Pokemon name="Carabaffe" id={8} />
         <Pokemon name="Tortank" id={9} />
