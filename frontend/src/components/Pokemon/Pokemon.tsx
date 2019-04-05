5a40bcc8dec993c52d0c55fdad15dcfb105b6a00
antkahn <ant.kahn@gmail.com>
2019-04-05 10:54:16 +0200

    pokemon as functionnal

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -11,19 +11,14 @@ interface Props {
 
 const pokeApiUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
 
-class Pokemon extends React.Component<Props> {
-  render() {
-    const { name, id, weight, height } = this.props;
-    return (
-      <Style.Wrapper>
-        <Style.Name>{name}</Style.Name>
-        <img src={`${pokeApiUrl}/${id}.png`} />
-        <Style.Attribute>Id: {id}</Style.Attribute>
-        <Style.Attribute>Weight: {weight / 10} kg</Style.Attribute>
-        <Style.Attribute>Height: {height * 10} cm</Style.Attribute>
-      </Style.Wrapper>
-    );
-  }
-}
+const Pokemon = ({ name, id, weight, height }: Props) => (
+  <Style.Wrapper>
+    <Style.Name>{name}</Style.Name>
+    <img src={`${pokeApiUrl}/${id}.png`} />
+    <Style.Attribute>Id: {id}</Style.Attribute>
+    <Style.Attribute>Weight: {weight / 10} kg</Style.Attribute>
+    <Style.Attribute>Height: {height * 10} cm</Style.Attribute>
+  </Style.Wrapper>
+);
 
 export default Pokemon;
