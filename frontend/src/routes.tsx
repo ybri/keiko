78aa79a5620e4ea46c718f40ddfc7049ce53c7a3
antkahn <ant.kahn@gmail.com>
2019-04-08 11:56:25 +0200

    add route pokemon

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,11 +2,13 @@ import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router';
 
 const Home = lazy(() => import('./pages/Home'));
+const Pokemon = lazy(() => import('./pages/Pokemon'));
 
 const routes = () => (
   <Suspense fallback={<div>Loading...</div>}>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/pokemon/:id" component={Pokemon} />
     </Switch>
   </Suspense>
 );
