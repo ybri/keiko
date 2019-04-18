b71788634d0922a901c07a1467c06eb3276acec7
Florian Gagnadre <florian.gagnadre@gmail.com>
2019-04-18 18:52:52 +0200

    Return hard-coded pokemon as JSON object

--- backend/src/Controller/PokemonController.php
+++ backend/src/Controller/PokemonController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use App\Entity\Pokemon;
 
 class PokemonController
 {
+    private $normalizer;
+
+    public function __construct(NormalizerInterface $normalizer) {
+        $this->normalizer = $normalizer;
+    }
+
     /**
      * @Route("/api/pokemon")
      */
-    public function sayHello(): Response
+    public function get(): JsonResponse
     {
-        return new Response("Hello World");
+        $pokemon = new Pokemon();
+        $pokemon->setId(1);
+        $pokemon->setName("bulbasaur");
+        $pokemon->setWeight(67);
+
+        $response = $this->normalizer->normalize($pokemon, 'json');
+
+        return new JsonResponse($response);
     }
 }
---
+++ backend/src/Entity/Pokemon.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+class Pokemon
+{
+    /**
+     * @var int $id
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     */
+    private $name;
+
+    /**
+     * @var int $weight
+     */
+    private $weight;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string $name
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int $weight
+     */
+    public function getWeight(): int
+    {
+        return $this->weight;
+    }
+
+    /**
+     * @param int $weight
+     */
+    public function setWeight(int $weight): void
+    {
+        $this->weight = $weight;
+    }
+}
