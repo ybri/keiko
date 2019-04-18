3ac8680f7f831fdf33ae518e783cafc464bc9f2a
Florian Gagnadre <florian.gagnadre@gmail.com>
2019-04-18 18:13:23 +0200

    Use annotations to declare routes

--- backend/src/Controller/PokemonController.php
+++ backend/src/Controller/PokemonController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PokemonController
 {
+    /**
+     * @Route("/api/pokemon")
+     */
     public function sayHello(): Response
     {
         return new Response("Hello World");
