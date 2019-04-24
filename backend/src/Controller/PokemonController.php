79f4fea385613b141fe84c9ef0275d3518a91e31
mollardthomas <mollardthomas@gmail.com>
2019-04-24 17:01:58 +0200

    Return hard coded pokemon in json

--- backend/src/Controller/PokemonController.php
+++ backend/src/Controller/PokemonController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use App\Entity\Pokemon;
 
 class PokemonController
 {
     /**
+     * @var NormalizerInterface
+     */
+    private $normalizer;
+
+    /**
+     * @param NormalizerInterface $normalizer
+     */
+    public function __construct(NormalizerInterface $normalizer) {
+        $this->normalizer = $normalizer;
+    }
+
+    /**
      * @Route("/pokemon")
      *
-     * @return Response
+     * @return JsonResponse
      */
-    public function helloWorld(): Response
+    public function get(): JsonResponse
     {
-        return new Response('Hello World');
+        $pokemon = new Pokemon();
+        $pokemon->setName('jean-michel');
+        $pokemon->setWeight(100);
+        $pokemon->setHeight(100);
+
+        $response = $this->normalizer->normalize($pokemon, 'json');
+
+        return new JsonResponse($response);
     }
 }
