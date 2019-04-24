7fb4cae1c31bfe0125f66a0dd4d450cc71df042d
mollardthomas <mollardthomas@gmail.com>
2019-04-24 17:50:12 +0200

    Refactor using ApiPlatform

--- backend/src/Entity/Pokemon.php
+++ backend/src/Entity/Pokemon.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
+ * @ApiResource
  * @ORM\Table(name="pokemon")
  * @ORM\Entity()
  */
