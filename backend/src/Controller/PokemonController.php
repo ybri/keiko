9efba37aced08d1932af3fb356483c325e194e9e
mollardthomas <mollardthomas@gmail.com>
2019-04-24 16:54:58 +0200

    Use symfony annotation for routing

--- backend/src/Controller/PokemonController.php
+++ backend/src/Controller/PokemonController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PokemonController
 {
     /**
+     * @Route("/pokemon")
+     *
      * @return Response
      */
     public function helloWorld(): Response
