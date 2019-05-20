f96f300c505559c92045df9567415737a375720f
Yann Briancon <yannb@sipios.com>
2019-05-20 13:55:38 +0200

    Add Pokemon component

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,32 +1,13 @@
 import * as React from 'react';
 
+import Pokemon from 'components/Pokemon';
 import Style from './Home.style';
 
 class Home extends React.Component {
   render(): React.ReactNode {
-    const pokemon = {
-      name: 'Carapuce',
-      number: 7,
-    };
-
     return (
       <Style.Intro>
-        <div>Bienvenue sur ton futur pokédex !</div>
-        <div>
-          Tu vas pouvoir apprendre tout ce qu'il faut sur React, Redux et Symfony, et attraper des
-          pokemons !
-        </div>
-        <div>
-          <p>Voici mon premier pokemon:</p>
-          <p>
-            {pokemon.name}: n°{pokemon.number}
-          </p>
-          <img
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-              pokemon.number
-            }.png`}
-          />
-        </div>
+        <Pokemon name="Carapuce" id={7} />
       </Style.Intro>
     );
   }
